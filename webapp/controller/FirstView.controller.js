855507c36486a4a7a3d9389ba0533772f954bff1
GururajSK <gururaj.k06@gmail.com>
2021-12-31 05:10:49 +0000

    First commit

---
+++ webapp/controller/FirstView.controller.js
@@ -0,0 +1,27 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller) {
+        "use strict";
+
+        return Controller.extend("my.ui.firstapp.myfirstproject.controller.FirstView", {
+            onInit: function () {
+
+            },
+
+            onBeforeRendering: function () {
+
+            },
+
+            onAfterRendering: function () {
+
+            },
+
+            onExit: function () {
+                
+            }
+        });
+    });
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"myui.firstapp./my_first_project/test/unit/controller/FirstView.controller"
+], function () {
+	"use strict";
+});
